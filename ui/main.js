b29acd64f45bf1fb83d7e522447bbafd980637aa
Balaji S <balajiyadhav0@gmail.com>
2017-08-24 01:42:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text")
-element.innerHTML = 'HI I am balaji aka 7r0nr007'
+var element = document.getElementById("main-text");
+element.innerHTML = 'HI I am balaji aka 7r0nr007';
+var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft+"px";
+}
+img.onclick = function(){
+    console.log('clicked')
+    var interval = setInterval(moveRight,50);
+    
+}
