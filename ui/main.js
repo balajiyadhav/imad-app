3ade01acb3352d0e84ca74930886180c83951953
Balaji S <balajiyadhav0@gmail.com>
2017-08-26 14:15:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function(){
     var list = '';
     console.log(names.length);
     for(var i =0; i < names.length; i++){
-        list += '<li>'+names[i]+'<li>';
+        list += '<li>'+names[i]+'</li>';
     }
     console.log(list);
     var ul = document.getElementById('namelist');
