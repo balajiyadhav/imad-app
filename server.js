6c9aaf9a4fd358528e033745fad5a55f0a767dce
Balaji S <balajiyadhav0@gmail.com>
2017-08-27 14:17:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.get('/submit-name', function (req,res){
     names.push(name);
     res.send(JSON.stringify(names));
 })
+var comments=[];
+app.get('/commentsa1', function (req,res){
+    var comment = req.query.comment;
+    comments.push(comment);
+    res.send(JSON.stringify(comments));
+})
 var articleOne = {
     title: 'Article One | Balaji S',
     heading: 'Article One',
