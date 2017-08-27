75f433013b8980854b08322069106c64dd0a9ac6
Balaji S <balajiyadhav0@gmail.com>
2017-08-27 14:10:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/articleone.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.js'));
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
