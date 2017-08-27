6b324dd3abb8a3ece0db9efc318d4db5d5b9ad01
Balaji S <balajiyadhav0@gmail.com>
2017-08-27 14:14:25 +0530

    [imad-console] Updates ui/articleone.js

--- ui/articleone.js
+++ ui/articleone.js
@@ -2,4 +2,31 @@ console.log("articleone js loaded");
 var submit = document.getElementById('a1submit_btn');
 submit.onclick = function(){
     console.log('Submit clicked');
+        //displaying name list
+     //Make a req to counter endpoint
+    var request = new XMLHttpRequest();
+    //get response in variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                //console.log('2nd if');
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                console.log(comments.length);
+                for(var i =0; i < comments.length; i++){
+                    list += '<li>'+comments[i]+'</li><hr/>';
+                }
+                console.log(list);
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+                
+            }
+        }
+    }
+    //render in correct span
+    var nameInput = document.getElementById('a1name');
+    var name1 = nameInput.value;
+   request.open('GET','/commenta1?comment='+name1,true);
+   request.send(null);
 }
