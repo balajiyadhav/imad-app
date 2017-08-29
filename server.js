6eeea3ac2d4c20e971d61fa19d8dc0408e13e1bd
Balaji S <balajiyadhav0@gmail.com>
2017-08-29 11:42:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require("pg").Pool;
 var config = {
     user:'balajiyadhav0',
     database:'balajiyadhav0',
-    host:'http://db.imad.hasura-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD,
 };
